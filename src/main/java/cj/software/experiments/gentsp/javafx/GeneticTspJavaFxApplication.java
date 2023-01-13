364f8e9958169a8f52c43c8cafaa33cb68ed2887
cjacobme <cjacobme@aol.com>
2023-01-13 19:56:57 +0100

    Create GeneticTspJavaFxApplication.java

---
+++ src/main/java/cj/software/experiments/gentsp/javafx/GeneticTspJavaFxApplication.java
@@ -0,0 +1,13 @@
+package cj.software.experiments.gentsp.javafx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneticTspJavaFxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneticTspJavaFxApplication.class, args);
+	}
+
+}
