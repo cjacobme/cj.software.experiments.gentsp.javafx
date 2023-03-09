2fccdb34da011d2730d49592e5d5ad2b787203a4
cjacobme <cjacobme@aol.com>
2023-03-09 21:39:07 +0100

    nextRandomValue with upper int border

--- src/main/java/cj/software/experiments/gentsp/util/RandomUtil.java
+++ src/main/java/cj/software/experiments/gentsp/util/RandomUtil.java
@@ -7,4 +7,8 @@ public class RandomUtil {
     public double nextRandomValue() {
         return Math.random();
     }
+
+    public int nextRandomValue(int max) {
+        return (int)(Math.random() * max);
+    }
 }
