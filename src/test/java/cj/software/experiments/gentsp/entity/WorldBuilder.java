9802601efe1ed309ed5e651afa510f980b6daf13
cjacobme <cjacobme@aol.com>
2023-03-09 20:34:12 +0100

    added new entity World

---
+++ src/test/java/cj/software/experiments/gentsp/entity/WorldBuilder.java
@@ -0,0 +1,7 @@
+package cj.software.experiments.gentsp.entity;
+
+public class WorldBuilder extends World.Builder{
+    public WorldBuilder() {
+        super.withWidth(180).withHeight(180);
+    }
+}
