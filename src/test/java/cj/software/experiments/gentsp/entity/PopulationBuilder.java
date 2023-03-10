11985d084d7e459f02907b0d0906555cd9523e5a
cjacobme <cjacobme@aol.com>
2023-03-10 18:11:20 +0100

    corrections of MR

--- src/test/java/cj/software/experiments/gentsp/entity/PopulationBuilder.java
+++ src/test/java/cj/software/experiments/gentsp/entity/PopulationBuilder.java
@@ -4,8 +4,14 @@ public class PopulationBuilder extends Population.Builder {
     public PopulationBuilder() {
         super.withIndividuals(
                         new IndividualBuilder().build(),
-                        Individual.builder().withChromosomes(new int[]{22, 21, 20, 19}).build(),
-                        Individual.builder().withChromosomes(new int[]{33, 32, 31, 30}).build())
+                        Individual.builder()
+                                .withPopulationCounter(3)
+                                .withCycleCounter(11)
+                                .withChromosomes(new int[]{22, 21, 20, 19}).build(),
+                        Individual.builder()
+                                .withPopulationCounter(1)
+                                .withCycleCounter(1234)
+                                .withChromosomes(new int[]{33, 32, 31, 30}).build())
                 .withPopulationFitness(22.342);
     }
 }
