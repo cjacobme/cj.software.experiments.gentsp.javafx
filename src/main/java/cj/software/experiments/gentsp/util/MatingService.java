a3a88fa10cb88cdb19f030f63e5592d979dbf3f7
cjacobme <cjacobme@aol.com>
2023-03-11 22:26:26 +0100

    select randomly a parent

--- src/main/java/cj/software/experiments/gentsp/util/MatingService.java
+++ src/main/java/cj/software/experiments/gentsp/util/MatingService.java
@@ -1,15 +1,21 @@
 package cj.software.experiments.gentsp.util;
 
 import cj.software.experiments.gentsp.entity.Individual;
+import cj.software.experiments.gentsp.entity.Population;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class MatingService {
+
     @Autowired
     private RandomUtil randomUtil;
+
+    @Autowired
+    private RatingCalculator ratingCalculator;
 
     Individual[] shuffle(Individual[] individuals) {
         Individual[] result = Arrays.copyOf(individuals, individuals.length);
@@ -19,6 +25,17 @@ public class MatingService {
             result[index] = result[iInd];
             result[iInd] = individual;
         }
+        return result;
+    }
+
+    Individual selectParent(Population population, int tournamentSize) {
+        Individual[] individuals = population.getIndividuals();
+        Individual[] shuffled = shuffle(individuals);
+        Individual[] tournament = new Individual[tournamentSize];
+        System.arraycopy(shuffled, 0, tournament, 0, tournamentSize);
+        Population temp = Population.builder().withIndividuals(tournament).build();
+        List<Individual> rated = ratingCalculator.sortFitness(temp);
+        Individual result = rated.get(0);
         return result;
     }
 }
--- src/test/java/cj/software/experiments/gentsp/util/MatingServiceTest.java
+++ src/test/java/cj/software/experiments/gentsp/util/MatingServiceTest.java
@@ -1,10 +1,13 @@
 package cj.software.experiments.gentsp.util;
 
 import cj.software.experiments.gentsp.entity.Individual;
+import cj.software.experiments.gentsp.entity.Population;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -40,7 +43,7 @@ class MatingServiceTest {
     void shuffle1() {
         // setup data
         Individual ind0 = createIndividual(10.0, 15.0, 0, 0, 0, 1, 2, 3, 4);
-        Individual ind1 = createIndividual(10.0, 10.0, 0, 1, 4, 3, 2, 1, 0);
+        Individual ind1 = createIndividual(12.0, 10.0, 0, 1, 4, 3, 2, 1, 0);
         Individual ind2 = createIndividual(15.0, 20.0, 0, 2, 1, 3, 4, 0, 2);
         Individual ind3 = createIndividual(1.0, 2.0, 0, 3, 0, 2, 1, 3, 4);
         Individual ind4 = createIndividual(11.0, 12.0, 0, 4, 0, 4, 3, 2, 1);
@@ -80,5 +83,66 @@ class MatingServiceTest {
         // checks
         Individual[] expected = new Individual[]{ind2, ind0, ind3, ind1};
         assertThat(shuffled).as("shuffled").isEqualTo(expected);
+    }
+
+    @Test
+    void selectParent1() {
+        // setup data
+        Individual ind0 = createIndividual(10.0, 15.0, 0, 0, 0, 1, 2, 3, 4);
+        Individual ind1 = createIndividual(12.0, 10.0, 0, 1, 4, 3, 2, 1, 0);
+        Individual ind2 = createIndividual(15.0, 20.0, 0, 2, 1, 3, 4, 0, 2);
+        Individual ind3 = createIndividual(1.0, 2.0, 0, 3, 0, 2, 1, 3, 4);
+        Individual ind4 = createIndividual(11.0, 12.0, 0, 4, 0, 4, 3, 2, 1);
+        Population population = Population.builder()
+                .withIndividuals(ind0, ind1, ind2, ind3, ind4)
+                .build();
+        int tournamentSize = 3;
+        Population tournamentPopulation = Population.builder()
+                .withIndividuals(ind3, ind4, ind1)
+                .build();
+        List<Individual> rated = List.of(ind4, ind3, ind1);
+
+        // configure mocks
+        when(randomUtil.nextRandomValue(5)).thenReturn(2);
+        when(randomUtil.nextRandomValue(4)).thenReturn(0);
+        when(randomUtil.nextRandomValue(3)).thenReturn(1);
+        when(randomUtil.nextRandomValue(2)).thenReturn(1);
+        when(ratingCalculator.sortFitness(tournamentPopulation)).thenReturn(rated);
+
+        // invoke
+        Individual parent = matingService.selectParent(population, tournamentSize);
+
+        // checks
+        assertThat(parent).as("selected parent").isEqualTo(ind4);
+    }
+
+    @Test
+    void selectParent2() {
+        // setup data
+        Individual ind0 = createIndividual(10.0, 15.0, 1, 0, 0, 1, 2, 3);
+        Individual ind1 = createIndividual(20.0, 30.0, 1, 1, 3, 2, 1, 0);
+        Individual ind2 = createIndividual(30.0, 35.0, 1, 2, 1, 2, 3, 0);
+        Individual ind3 = createIndividual(40.0, 40.0, 1, 3, 2, 1, 3, 0);
+        Population population = Population.builder()
+                .withIndividuals(ind3, ind2, ind1, ind0)
+                .build();
+        int tournamentSize = 2;
+        Population tournamentPopulation = Population.builder()
+                .withIndividuals(ind2, ind0)
+                .build();
+        List<Individual> rated = List.of(ind0, ind2);
+
+        // configure mocks
+        when(randomUtil.nextRandomValue(4)).thenReturn(2);
+        when(randomUtil.nextRandomValue(3)).thenReturn(0);
+        when(randomUtil.nextRandomValue(2)).thenReturn(0);
+        when(ratingCalculator.sortFitness(tournamentPopulation)).thenReturn(rated);
+
+        // invoke
+        Individual parent = matingService.selectParent(population, tournamentSize);
+
+        // checks
+        assertThat(parent).as("selected parent").isEqualTo(ind0);
+
     }
 }
