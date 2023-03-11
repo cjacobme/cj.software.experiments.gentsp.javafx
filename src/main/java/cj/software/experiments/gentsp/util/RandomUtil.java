2fda25d2b7ff269b1a2667ee9aa5839e36454545
cjacobme <cjacobme@aol.com>
2023-03-11 22:05:10 +0100

    next double value

--- src/main/java/cj/software/experiments/gentsp/util/RandomUtil.java
+++ src/main/java/cj/software/experiments/gentsp/util/RandomUtil.java
@@ -8,6 +8,10 @@ import java.util.Random;
 public class RandomUtil {
     private final Random random = new Random();
 
+    public double nextRandomValue() {
+        return random.nextDouble();
+    }
+
     public int nextRandomValue(int max) {
         return random.nextInt(max);
     }
